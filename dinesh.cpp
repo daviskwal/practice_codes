541026b9c34d90dc13d5e0b81f04c763da1b9a90
Dinesh Khandelwal <dinesh.kwal@gmail.com>
2014-09-06 21:22:07 +0530

    Created a new file

---
+++ dinesh.cpp
@@ -0,0 +1,42 @@
+// Copyright [2014] <Strike>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <climits>
+#include <cctype>
+#include <sstream>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+#define MOD 1000000009
+#define mp(a, b) make_pair(a, b)
+#define pb(a) push_back(a)
+#define toDigit(c) (c - 'a')
+#define isEven(c) !(c & 1)
+#define isOdd(c) (c & 1)
+#define lineBreak printf("\n")
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long> vl;
+typedef map<int, int> mii;
+
+int main() {
+  cout << "Dinesh\n";
+  return 0;
+}
